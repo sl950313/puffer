56b18e59352edf2381cf113d87c35d732e92d868
James Hong <james.hong@cs.stanford.edu>
2018-01-18 14:50:11 -0800

    Add audio to the streaming over websockets demo

--- src/media-server/static/ws_mse.js
+++ src/media-server/static/ws_mse.js
@@ -2,6 +2,15 @@
 const ws = new WebSocket('ws://localhost:8080');
 ws.binaryType = 'arraybuffer';
 
+// media sources
+const video_ms = new MediaSource();
+const video = document.getElementById('tv-player');
+video.src = window.URL.createObjectURL(video_ms);
+
+const audio_ms = new MediaSource();
+const audio = document.getElementById('tv-audio');
+audio.src = window.URL.createObjectURL(audio_ms);
+
 var video_buffer;
 var pending_video_chunks = [];
 
@@ -18,41 +27,46 @@ function parse_message(data) {
 }
 
 function handle_message(e) {
-  // console.log(ms.activeSourceBuffers);
   var message = parse_message(e.data);
-  console.log(message.header);
+  console.log(message.header.type, message.header.quality);
   if (message.header.type == 'audio-init') {
-    // audio_buffer = ms.addSourceBuffer(message.header.mimeCodec);
-    // audio_buffer.appendBuffer(message.data);
+    if (!audio_buffer) {
+      audio_buffer = audio_ms.addSourceBuffer(message.header.mimeCodec);
+      audio_buffer.mode = 'sequence';
+      audio_buffer.addEventListener('updateend', function(e) {
+        if (!audio_buffer.updating && pending_audio_chunks.length > 0) {
+          audio_buffer.appendBuffer(pending_audio_chunks.shift());
+        }
+      });
+      // audio_buffer.addEventListener('updatestart', function(e) {});
+      audio_buffer.addEventListener('error', function(e) {
+        console.log('error', e);
+      });
+      audio_buffer.addEventListener('abort', function(e) {
+        console.log('abort', e);
+      });
+      // audio_buffer.addEventListener('update', function(e) {});
+    }
+    pending_audio_chunks.push(message.data);
   } else if (message.header.type == 'audio-chunk') {
-    // if (!audio_buffer.updating) {
-    //   audio_buffer.appendBuffer(message.data);
-    // } else {
-    //   pending_audio_chunks.push(message.data);
-    // }
-    // audio_buffer.addEventListener('updateend', function() {
-    //   if (pending_audio_chunks.length > 0) {
-    //     audio_buffer.appendBuffer(pending_audio_chunks.shift());
-    //   }
-    // });
+    pending_audio_chunks.push(message.data);
   } else if (message.header.type == 'video-init') {
     if (!video_buffer) {
-      video_buffer = ms.addSourceBuffer(message.header.mimeCodec);
+      video_buffer = video_ms.addSourceBuffer(message.header.mimeCodec);
       video_buffer.mode = 'sequence';
-
       video_buffer.addEventListener('updateend', function(e) {
-        if (pending_video_chunks.length > 0) {
+        if (!video_buffer.updating && pending_video_chunks.length > 0) {
           video_buffer.appendBuffer(pending_video_chunks.shift());
         }
       });
-      video_buffer.addEventListener('updatestart', function(e) {});
+      // video_buffer.addEventListener('updatestart', function(e) {});
       video_buffer.addEventListener('error', function(e) {
         console.log('error', e);
       });
       video_buffer.addEventListener('abort', function(e) {
         console.log('abort', e);
       });
-      video_buffer.addEventListener('update', function(e) {});
+      // video_buffer.addEventListener('update', function(e) {});
     }
     pending_video_chunks.push(message.data);
   } else if (message.header.type == 'video-chunk') {
@@ -63,16 +77,35 @@ function handle_message(e) {
       && pending_video_chunks.length > 0) {
     video_buffer.appendBuffer(pending_video_chunks.shift());
   }
+
+  if (audio_buffer && !audio_buffer.updating
+      && pending_audio_chunks.length > 0) {
+    audio_buffer.appendBuffer(pending_audio_chunks.shift());
+  }
 }
 
-// media source
-const ms = new MediaSource();
+video.onpause = function() {
+  audio.pause();
+}
 
-const video = document.getElementById('tv-player');
-video.src = window.URL.createObjectURL(ms);
+video.onplay = function() {
+  audio.play();
+}
 
-ms.addEventListener('sourceopen', function(e) {
-  console.log('sourceopen: ' + ms.readyState);
+video.onclick = function () {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+video.onvolumechange = function () {
+  audio.volume = video.volume;
+}
+
+video_ms.addEventListener('sourceopen', function(e) {
+  console.log('sourceopen: ' + video_ms.readyState);
 
   ws.onmessage = handle_message
 
@@ -89,27 +122,51 @@ ms.addEventListener('sourceopen', function(e) {
     if (video_buffer.buffered.length > 0) {
       ws.send(JSON.stringify({
         type: 'client-vbuf',
-        buffered: video_buffer.buffered.end(0) - video.currentTime
+        bufferLength: video_buffer.buffered.end(0) - video.currentTime
       }))
     }
     setTimeout(send_vbuf_info, 1000);
   }
 
+  function send_abuf_info() {
+    if (audio_buffer.buffered.length > 0) {
+      ws.send(JSON.stringify({
+        type: 'client-abuf',
+        bufferLength: audio_buffer.buffered.end(0) - audio.currentTime
+      }))
+    }
+    setTimeout(send_abuf_info, 2000);
+  }
+
   setTimeout(function() {
     video.play();
+    audio.play();
     send_vbuf_info();
+    send_abuf_info();
   }, 200);
 });
 
 // other media source event listeners for debugging
-ms.addEventListener('sourceended', function(e) {
-  console.log('sourceended: ' + ms.readyState);
+video_ms.addEventListener('sourceended', function(e) {
+  console.log('sourceended: ' + video_ms.readyState);
 });
 
-ms.addEventListener('sourceclose', function(e) {
-  console.log('sourceclose: ' + ms.readyState);
+video_ms.addEventListener('sourceclose', function(e) {
+  console.log('sourceclose: ' + video_ms.readyState);
 });
 
-ms.addEventListener('error', function(e) {
-  console.log('media source error: ' + ms.readyState);
+video_ms.addEventListener('error', function(e) {
+  console.log('media source error: ' + video_ms.readyState);
+});
+
+audio_ms.addEventListener('sourceended', function(e) {
+  console.log('sourceended: ' + audio_ms.readyState);
+});
+
+audio_ms.addEventListener('sourceclose', function(e) {
+  console.log('sourceclose: ' + audio_ms.readyState);
+});
+
+audio_ms.addEventListener('error', function(e) {
+  console.log('media source error: ' + audio_ms.readyState);
 });
